73ed2f0a9459f2ba528d2dd5d36c22cff8a45859
pvsafwa <135609250+pvsafwa@users.noreply.github.com>
2023-12-21 22:15:39 +0530

    Modified Slab-5 Tax Calculation

--- IncomeTaxCalculatorForSelfEmployd/src/com/CalculateSlab5Tax.java
+++ IncomeTaxCalculatorForSelfEmployd/src/com/CalculateSlab5Tax.java
@@ -1,50 +1,35 @@
 package com;
 
 public class CalculateSlab5Tax {
-	float salary;
-	
-	public void calculateSlab5Tax(float annualSalary) {
-		
-		salary = annualSalary;
-	
-	
-	if (annualSalary > 1500000 && annualSalary <= 5000000)
-	{
-	System.out.println("Congratulatons! You comes under tax Slab-5");
-	float slab5Tax = 15000 + 30000 + 45000 + 60000 +((annualSalary - 1500000) * 30/100);
-	System.out.println("Your annual tax is " +slab5Tax);
-	float slab5Cess = slab5Tax*4/100;
-	System.out.println("Your annual heath and education cess is " +slab5Cess);
-	float slab5Total = slab5Tax + slab5Cess;
-	System.out.println("The final tax liability for the year is " +slab5Total);
-	}
-	
-	else if (annualSalary > 5000000 && annualSalary < 10000000)
-	{
-	System.out.println("Congratulatons! You comes under tax Slab-5");
-	float slab5Tax = 15000 + 30000 + 45000 + 60000 +((annualSalary - 1500000) * 30/100);
-	System.out.println("Your annual tax is " +slab5Tax);
-	float slab5Cess = slab5Tax*4/100;
-	System.out.println("Your annual heath and education cess is " +slab5Cess);
-	System.out.println("Since your annual salary is more than 50 lakhs, you  have to pay a surchargeof 10% of the tax.");
-	float surCharge = slab5Tax *(10/100);
-	System.out.println("Your surcharge payable is " +surCharge);
-	float slab5Total = slab5Tax + slab5Cess + surCharge;
-	System.out.println("The final tax liability for the year is " +slab5Total);
-	}
-	
-	else if (annualSalary > 10000000)
-	{
-	System.out.println("Congratulatons! You comes under tax Slab-5");
-	float slab5Tax = 15000 + 30000 + 45000 + 60000 +((annualSalary - 1500000) * 30/100);
-	System.out.println("Your annual tax is " +slab5Tax);
-	float slab5Cess = slab5Tax*4/100;
-	System.out.println("Your annual heath and education cess is " +slab5Cess);
-	System.out.println("Since your annual salary is more than 1 Crore, you  have to pay a surchargeof 15% of the tax.");
-	float surCharge = 15/100*slab5Tax;
-	System.out.println("Your surcharge payable is " +surCharge);
-	float slab5Total = slab5Tax + slab5Cess + surCharge;
-	System.out.println("The final tax liability for the year is " +slab5Total);
-	}
-}
+    double salary;
+
+    public void calculateSlab5Tax(double annualSalary) {
+        salary = annualSalary;
+
+        final double SLAB_5_START = 1500000;
+        final double SLAB_5_MIDDLE = 5000000;
+        final double SLAB_5_TAX_RATE = 30.0;
+        final double CESS_RATE = 0.04;
+
+        double slab5Tax = 15000 + 30000 + 45000 + 60000 + ((annualSalary - SLAB_5_START) * SLAB_5_TAX_RATE / 100);
+        System.out.println("Your annual tax is " + slab5Tax);
+
+        double slab5Cess = slab5Tax * CESS_RATE;
+        System.out.println("Your annual health and education cess is " + slab5Cess);
+
+        double surCharge = 0.0;
+
+        if (annualSalary > SLAB_5_MIDDLE && annualSalary < 10000000) {
+            System.out.println("Since your annual salary is more than 50 lakhs, you have to pay a surcharge of 10% of the tax.");
+            surCharge = slab5Tax * 0.10;
+        } else if (annualSalary > 10000000) {
+            System.out.println("Since your annual salary is more than 1 Crore, you have to pay a surcharge of 15% of the tax.");
+            surCharge = slab5Tax * 0.15;
+        }
+
+        System.out.println("Your surcharge payable is " + surCharge);
+
+        double slab5Total = slab5Tax + slab5Cess + surCharge;
+        System.out.println("The final tax liability for the year is " + slab5Total);
+    }
 }
